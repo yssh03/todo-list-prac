c8c63dc08164e8f93eed3582cc5680745f4adcf4
yash.patel1 <yash.patel@drcsystems.com>
2022-12-20 19:03:42 +0530

    added env file

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
 
 mongoose.set("strictQuery", true);
 mongoose
   .connect(
-    "mongodb+srv://yash0310:drc1234@cluster0.qc3e0p4.mongodb.net/serverless-demo",
+    `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.qc3e0p4.mongodb.net/serverless-demo`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
